d7200da395801ffeda21fa4f5aef1dfab183ca9e
Heroic-Brine <stefanotoft101112@live.dk>
2020-03-25 22:35:18 +0100

    Working 1 but requires enter

--- Heroic-AD-Toolkit-Menu.ps1
+++ Heroic-AD-Toolkit-Menu.ps1
@@ -1,11 +1,48 @@
-function sohw-menu
+function show-menu
 {
     param(
         [string]$title = "Heroic's AD Toolkit"
     )
-}
-cls
-write-host "================$title================"
 
-write-host "1: Press 1 for computer enrollment."
-write-host "1:
+Clear-Host
+Write-Host "================$title================"
+
+Write-Host "1: Press 1 for computer enrollment."
+Write-Host "2: Press 2 for computer disenrolment."
+Write-Host "3: Press 3 for group policy update"
+Write-Host "4: Press 4 for Administrator group policy update"
+Write-Host "Q: Press Q to quit"
+}
+
+do {
+    show-menu
+    $input = Read-Host "What do you desire"
+    switch ($input) 
+            {  
+            '1'{
+                Clear-Host
+                'Option #1 it is'
+                Start-Sleep 2
+            }
+            '2'{
+                Clear-Host
+                'Option #2 it is'
+                Start-Sleep 2
+            }
+            '3'{
+                Clear-Host
+                'Option #3 it is'
+                Start-Sleep 2
+            }
+            '4'{
+                Clear-Host
+                'Option #4 it is'
+                Start-Sleep 2
+            }
+            'q'{
+                'Goodbye'
+                Start-Sleep 2
+                Exit
+            }
+            }
+} until ($input -eq 'q')
