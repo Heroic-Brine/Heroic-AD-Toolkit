d5ca972c83d945f128ff136665ed68a3282dcd70
Heroic-Brine <stefanotoft101112@live.dk>
2020-03-25 22:52:20 +0100

    Works with a single keypress

--- Heroic-AD-Toolkit-Menu.ps1
+++ Heroic-AD-Toolkit-Menu.ps1
@@ -16,27 +16,34 @@ Write-Host "Q: Press Q to quit"
 
 do {
     show-menu
-    $input = Read-Host "What do you desire"
-    switch ($input) 
+    Write-Host "What do you desire?"
+    Write-Host ""
+    $input = [System.Console]::ReadKey("What do you desire") 
+    $k = $input.key
+    switch ($k) 
             {  
-            '1'{
+            'd1'{
                 Clear-Host
                 'Option #1 it is'
+                'computer enrollment opening'
                 Start-Sleep 2
             }
-            '2'{
+            'd2'{
                 Clear-Host
                 'Option #2 it is'
+                'computer disenrolment opening'
                 Start-Sleep 2
             }
-            '3'{
+            'd3'{
                 Clear-Host
                 'Option #3 it is'
+                'Running Group policy update immediately'
                 Start-Sleep 2
             }
-            '4'{
+            'd4'{
                 Clear-Host
                 'Option #4 it is'
+                'please accept UAC dialog'
                 Start-Sleep 2
             }
             'q'{
