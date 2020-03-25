6d4220d4d01327d56ba204db7132c70bc67a1431
Heroic-Brine <stefanotoft101112@live.dk>
2020-03-25 21:52:07 +0100

    Inital

---
+++ Heroic-AD-Toolkit-Menu.ps1
@@ -0,0 +1,11 @@
+function sohw-menu
+{
+    param(
+        [string]$title = "Heroic's AD Toolkit"
+    )
+}
+cls
+write-host "================$title================"
+
+write-host "1: Press 1 for computer enrollment."
+write-host "1:
